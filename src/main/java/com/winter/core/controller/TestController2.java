dd03b8badb12dbc22fd9880567ef4b2f83ec2af5
904917125 <904917125@qq.com>
2018-05-04 17:01:49 +0800

    Signed-off-by: 904917125 <904917125@qq.com>

---
+++ src/main/java/com/winter/core/controller/TestController2.java
@@ -0,0 +1,35 @@
+package com.winter.core.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.winter.annotation.MyController;
+import com.winter.annotation.MyRequestMapping;
+import com.winter.annotation.MyRequestParam;
+
+@MyController
+@MyRequestMapping("/test1")
+public class TestController2 {
+
+     @MyRequestMapping("/doTest")
+    public void test1(HttpServletRequest request, HttpServletResponse response,
+            @MyRequestParam("param") String param){
+        System.out.println(param);
+        try {
+            response.getWriter().write( "doTest method success! param:"+param);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+     @MyRequestMapping("/doTest2")
+    public void test2(HttpServletRequest request, HttpServletResponse response){
+        try {
+            response.getWriter().println("doTest2 method success!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
